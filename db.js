4c84e8131b2d175a33e920e8a23e6f91e57e3f09
talukdarnisan <talukdarnisan@gmail.com>
2024-09-10 13:37:52 +0600

    Update db.js

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
 const mysql = require('mysql');
 
+// Create a MySQL connection pool
 const pool = mysql.createPool({
   connectionLimit: 10,
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,  // Default MySQL port
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: {
-    rejectUnauthorized: false // Adjust this based on your SSL configuration
+    rejectUnauthorized: false // Adjust if SSL is required
   }
 });
 
+// Function to get a database connection
 const getConnection = (callback) => {
   pool.getConnection((err, connection) => {
     if (err) {
