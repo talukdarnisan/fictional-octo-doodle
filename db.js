965e1271de9eff9a301d4fab4f02226e57bad54d
talukdarnisan <talukdarnisan@gmail.com>
2024-09-13 21:10:06 +0600

    Update db.js

--- db.js
+++ db.js
@@ -7,7 +7,7 @@ const connection = mysql.createConnection({
     user: 'mysql',
     password: 'i1p662CepxK2LNwWUp5GPvvsl6B9cAU0zsrdGbx9sOwdjz4hh3jjlvqoeRqe7kiw',
     database: 'default',
-    port: 3306
+    port: 4532
 });
 
 // Connect to the database
