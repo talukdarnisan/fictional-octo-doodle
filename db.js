069ccd9efbcbc0f8698fce29d866fbdf5ec0aeb3
talukdarnisan <talukdarnisan@gmail.com>
2024-09-13 21:03:30 +0600

    Create db.js

---
+++ db.js
@@ -0,0 +1,23 @@
+// db.js
+const mysql = require('mysql2');
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: 'g8ooskkwossok488s4scg00s',
+    user: 'mysql',
+    password: 'i1p662CepxK2LNwWUp5GPvvsl6B9cAU0zsrdGbx9sOwdjz4hh3jjlvqoeRqe7kiw',
+    database: 'default',
+    port: 3306
+});
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) {
+        console.error('Database not connected:', err.message);
+    } else {
+        console.log('Database connected successfully');
+    }
+});
+
+// Export the connection
+module.exports = connection;
